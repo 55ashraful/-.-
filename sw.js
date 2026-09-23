e0874224c6110c9ba59a52489a5ce51981db59cf
55ashraful <ashrafulhaqedulon@gmail.com>
2026-09-23 20:18:49 +0600

    Change CACHE_NAME to 'sthai-khata-v3'
    Updated cache name to version 3 for service worker.

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
-const CACHE_NAME = 'sthai-khata-v1';
+const CACHE_NAME = 'sthai-khata-v3';
 const assets = [
   './index.html',
   './manifest.json'
 ];
 
-// ইনস্টল করার সময় ফাইলগুলো ক্যাশ করে নেওয়া
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -13,7 +12,6 @@ self.addEventListener('install', e => {
   );
 });
 
-// অফলাইনে থাকার সময় ক্যাশ থেকে পেজ লোড করা
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(response => {
