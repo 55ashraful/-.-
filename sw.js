72e11dcae606b4dbc36852ad946b1f7a094ee290
55ashraful <ashrafulhaqedulon@gmail.com>
2026-09-25 04:28:59 +0600

    Update service worker cache version and fetch handling
    Updated the service worker to use a new cache version and added handling for navigation requests.

--- sw.js
+++ sw.js
@@ -1,21 +1,45 @@
-const CACHE_NAME = 'sthai-khata-v3';
+const CACHE_NAME = 'sthai-khata-v4';
 const assets = [
+  './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
-    })
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, copy));
+        return res;
+      }).catch(() => {
+        return caches.match(e.request).then(r => r || caches.match('./index.html'));
+      })
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(response => {
+        return response || fetch(e.request);
+      })
+    );
+  }
 });
